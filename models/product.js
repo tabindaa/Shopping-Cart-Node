775a9f9d13f92b739899300e825f29ac556be827
tabindaa <tabindaa1@gmail.com>
2024-08-29 12:03:41 +1000

    MVC architecture

--- models/product.js
+++ models/product.js
@@ -3,36 +3,33 @@ const path = require("path");
 
 const rootDir = require("../utils/path");
 
-const products = [];
+const p = path.join(rootDir, "data", "products.json");
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
   constructor(title) {
     this.title = title;
   }
-  save() {
-    const p = path.join(rootDir, "data", "products.json");
-    fs.readFile(p, (err, fileContent) => {
-      console.log(fileContent);
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
 
+  save() {
+    getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
-        console.log;
+        console.log(err);
       });
     });
   }
 
   static fetchAll(cb) {
-    const p = path.join(rootDir, "data", "products.json");
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        cb([]);
-      }
-      if (fileContent) cb(JSON.parse(fileContent));
-    });
-    return products;
+    getProductsFromFile(cb);
   }
 };
